b838974912bc35a53b29e49020500fa19c5724f9
nilauth <nilauth.dev@gmail.com>
2024-03-15 17:07:46 +0000

    authorize reqs with token abilities

--- app/Http/Requests/StoreCustomerRequest.php
+++ app/Http/Requests/StoreCustomerRequest.php
@@ -10,6 +10,10 @@ class StoreCustomerRequest extends FormRequest {
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool {
+        $user = $this->user();
+
+        return $user != null && $user->tokenCan( 'create' );
+
         return true;
     }
 
